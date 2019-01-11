2be5df65b32d5b5070a5b9678f744c4cc3c34348
Jessica Bee <bee.jess@gmail.com>
2019-01-11 01:46:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
-main.remove()
+document.main.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
-document.body.appendChild()
+const newHeader = document.createElement("h1");
+newHeader.id="victory"
+document.body.appendChild(newHeader)
