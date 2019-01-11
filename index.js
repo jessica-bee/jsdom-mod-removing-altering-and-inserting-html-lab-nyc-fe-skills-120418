bba0cd756b13c582d385bca89969cbcc1ee42f2b
Jessica Bee <bee.jess@gmail.com>
2019-01-11 01:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
 
+element.remove(main)
+
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+document.createElement(h1)
+element.h1 = "Jessica is the champion!"
